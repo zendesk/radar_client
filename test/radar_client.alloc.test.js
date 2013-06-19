9fa8c6262181f54864d2f991d1bb245894a0c444
Sam Shull <sshull@zendesk.com>
2013-06-19 11:51:43 -0700

    Separate StateMachine Client dependency
    Make the state machine unaware of the client and set up event handling
    in Client class. Add some unit tests.

--- test/radar_client.alloc.test.js
+++ test/radar_client.alloc.test.js
@@ -1,25 +1,26 @@
 var assert = require('assert'),
-
     MockEngine = require('./lib/engine.js'),
-    RadarClient = require('../lib/radar_client.js');
+    RadarClient = require('../lib/radar_client.js'),
+    client;
 
 RadarClient.setBackend(MockEngine);
 
 exports['given an instance of Radar client'] = {
-  afterEach: function(done) {
+  beforeEach: function() {
+    client = new RadarClient();
+  },
+
+  afterEach: function() {
     MockEngine.current._written = [];
-    done();
   },
 
   'calls to operations do not cause errors before the client is configured, but dont write either': function(done) {
-    var client = new RadarClient();
     client.status('test/foo').set('bar');
     assert.equal(MockEngine.current._written.length, 0);
     done();
   },
 
   'as long as the client is configured, any operation that requires a send will automatically connect': function(done) {
-    var client = new RadarClient();
     client.configure({ userId: 123, accountName: 'dev' });
     client.status('test/foo').set('bar', function() {
       assert.equal(MockEngine.current._written.length, 1);
@@ -28,32 +29,30 @@ exports['given an instance of Radar client'] = {
   },
 
   'alloc calls perform a connect if not connected': function(done) {
-    var client = new RadarClient();
     client.configure({ userId: 123, accountName: 'dev' });
     client.alloc('foo');
     setTimeout(function() {
       // we use a setTimeout, because connecting with the fake backend
       // is also async, it just takes 5 ms rather than a real connect duration
-      assert.ok(client.manager.is('ready'));
+      assert.ok(client.manager.is('activated'));
       done();
     }, 10);
   },
 
   'multiple alloc calls just trigger the callback': function(done) {
-    var client = new RadarClient(),
-        readyCount = 0,
+    var readyCount = 0,
         allocDoneCount = 0;
-    client.configure({ userId: 123, accountName: 'dev' });
     client.on('ready', function() {
       readyCount++;
     });
+    client.configure({ userId: 123, accountName: 'dev' });
     // also test that the callback param works
     function onAlloc() {
       allocDoneCount++;
     }
     client.alloc('foo', onAlloc);
     setTimeout(function() {
-      assert.ok(client.manager.is('ready')); // = Ready state
+      assert.ok(client.manager.is('activated')); // = Ready state
       assert.equal(readyCount, 3);
       assert.equal(allocDoneCount, 1);
       // if the connect code would trigger, then these would
@@ -71,7 +70,6 @@ exports['given an instance of Radar client'] = {
   },
 
   'dealloc has no effect until all the allocs have been performed': function(done) {
-    var client = new RadarClient();
     client.configure({ userId: 123, accountName: 'dev' });
 
     client.alloc('foo');
@@ -79,10 +77,10 @@ exports['given an instance of Radar client'] = {
     client.alloc('baz');
     client.dealloc('baz');
     setTimeout(function() {
-      assert.ok(client.manager.is('ready')); // = Ready state
+      assert.ok(client.manager.is('activated')); // = Ready state
       client.dealloc('bar');
       setTimeout(function() {
-        assert.ok(client.manager.is('ready')); // = Ready state
+        assert.ok(client.manager.is('activated')); // = Ready state
         client.dealloc('foo');
         setTimeout(function() {
           assert.ok(client.manager.is('closed')); // = Stopped state
