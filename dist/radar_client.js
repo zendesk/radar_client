bd2aee1cc10a4b2df10ee800079e8fd77ab96ab4
Sam Shull <sshull@zendesk.com>
2013-06-26 14:20:27 -0700

    Remove the configure state from the state machine
    RADAR-61

--- dist/radar_client.js
+++ dist/radar_client.js
@@ -105,10 +105,7 @@ Client.prototype.configure = function(hash) {
   var configuration = hash || this._configuration || { accountName: '', userId: 0, userType: 0 };
   configuration.userType = configuration.userType || 0;
   this._configuration = this._me = configuration;
-
-  if (hash !== false) {
-    this.manager.configure();
-  }
+  this._isConfigured = !!hash;
   return this;
 };
 
@@ -269,7 +266,7 @@ Client.prototype._createManager = function() {
 Client.prototype._sendMessage = function(message) {
   if (this._socket && this.manager.is('activated')) {
     this._socket.sendPacket('message', JSON.stringify(message));
-  } else if (this.manager.hasBeen('configured')) {
+  } else if (this._isConfigured) {
     this._queue(message);
     this.manager.connectWhenAble();
   }
@@ -352,8 +349,7 @@ function create() {
     },
 
     events: [
-      { name: 'configure',     from: Machine.WILDCARD, to: 'configured' },
-      { name: 'connect',       from: [ 'configured', 'disconnected' ], to: 'connecting' },
+      { name: 'connect',       from: [ 'opened', 'disconnected' ], to: 'connecting' },
       { name: 'established',   from: 'connecting', to: 'connected' },
       { name: 'authenticate',  from: 'connected', to: 'authenticating' },
       { name: 'activate',      from: [ 'authenticating', 'activated' ], to: 'activated' },
@@ -371,21 +367,10 @@ function create() {
       },
 
       onstate: function(state, from, to) {
-        this.previousStates = this.previousStates || {};
-        this.previousStates[from] = 1;
-
         log.debug('state-' + state + ', from: ' + from + ', to: ' + to, Array.prototype.slice.call(arguments));
 
         this.emit('enterState', state);
         this.emit(state, arguments);
-      },
-
-      onconfigure: function() {
-        if (this.can('connect')) {
-          this.connect();
-        } else {
-          return false;
-        }
       },
 
       onconnect: function() {
@@ -450,10 +435,6 @@ function create() {
 
   machine.guardDelay = function() {
     return backoff.get() + 6000;
-  };
-
-  machine.hasBeen = function(state) {
-    return this.previousStates && this.previousStates[state];
   };
 
   machine.connectWhenAble = function() {
@@ -525,7 +506,86 @@ M.mixin = function(dest) {
 };
 module.exports = M;
 }};
-require.m[2] = { "/state-machine.js": function(module, exports, require){/*
+require.m[2] = { "/demo/demo.js": function(module, exports, require){Demo = function() {
+
+  var output = document.getElementById('output'),
+      demo   = document.getElementById('demo'),
+      panic  = document.getElementById('panic'),
+      warn   = document.getElementById('warn'),
+      calm   = document.getElementById('calm'),
+      clear  = document.getElementById('clear'),
+      count  = 0;
+
+  var log = function(msg, separate) {
+    count = count + (separate ? 1 : 0);
+    output.value = count + ": " + msg + "\n" + (separate ? "\n" : "") + output.value;
+    demo.className = fsm.current;
+    panic.disabled = fsm.cannot('panic');
+    warn.disabled  = fsm.cannot('warn');
+    calm.disabled  = fsm.cannot('calm');
+    clear.disabled = fsm.cannot('clear');
+  };
+
+  var fsm = StateMachine.create({
+
+    events: [
+      { name: 'start', from: 'none',   to: 'green'  },
+      { name: 'warn',  from: 'green',  to: 'yellow' },
+      { name: 'panic', from: 'green',  to: 'red'    },
+      { name: 'panic', from: 'yellow', to: 'red'    },
+      { name: 'calm',  from: 'red',    to: 'yellow' },
+      { name: 'clear', from: 'red',    to: 'green'  },
+      { name: 'clear', from: 'yellow', to: 'green'  },
+    ],
+
+    callbacks: {
+      onbeforestart: function(event, from, to) { log("STARTING UP"); },
+      onstart:       function(event, from, to) { log("READY");       },
+
+      onbeforewarn:  function(event, from, to) { log("START   EVENT: warn!",  true);  },
+      onbeforepanic: function(event, from, to) { log("START   EVENT: panic!", true);  },
+      onbeforecalm:  function(event, from, to) { log("START   EVENT: calm!",  true);  },
+      onbeforeclear: function(event, from, to) { log("START   EVENT: clear!", true);  },
+
+      onwarn:        function(event, from, to) { log("FINISH  EVENT: warn!");         },
+      onpanic:       function(event, from, to) { log("FINISH  EVENT: panic!");        },
+      oncalm:        function(event, from, to) { log("FINISH  EVENT: calm!");         },
+      onclear:       function(event, from, to) { log("FINISH  EVENT: clear!");        },
+
+      onleavegreen:  function(event, from, to) { log("LEAVE   STATE: green");  },
+      onleaveyellow: function(event, from, to) { log("LEAVE   STATE: yellow"); },
+      onleavered:    function(event, from, to) { log("LEAVE   STATE: red");    async(to); return StateMachine.ASYNC; },
+
+      ongreen:       function(event, from, to) { log("ENTER   STATE: green");  },
+      onyellow:      function(event, from, to) { log("ENTER   STATE: yellow"); },
+      onred:         function(event, from, to) { log("ENTER   STATE: red");    },
+
+      onchangestate: function(event, from, to) { log("CHANGED STATE: " + from + " to " + to); }
+    }
+  });
+
+  var async = function(to) {
+    pending(to, 3);
+    setTimeout(function() {
+      pending(to, 2);
+      setTimeout(function() {
+        pending(to, 1);
+        setTimeout(function() {
+          fsm.transition(); // trigger deferred state transition
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  };
+
+  var pending = function(to, n) { log("PENDING STATE: " + to + " in ..." + n); };
+
+  fsm.start();
+  return fsm;
+
+}();
+
+},
+"/state-machine.js": function(module, exports, require){/*
 
   Javascript State Machine Library - https://github.com/jakesgordon/javascript-state-machine
 
