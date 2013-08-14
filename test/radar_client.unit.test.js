2a6ddd988e480a9b3bb4d1bfc5c1dde808e8460b
Sam Shull <sshull@zendesk.com>
2013-08-14 16:44:21 -0700

    Issue #12: RadarClient listens to messages from closed socket
    - Remove all message listeners from the socket when it is closed
    - Only allow the socket to 'open' once
    
    closes #12

--- test/radar_client.unit.test.js
+++ test/radar_client.unit.test.js
@@ -632,6 +632,49 @@ exports.RadarClient = {
     },
 
     '._createManager': {
+      'should create a manager that cannot open the same socket twice': function() {
+        var never_called_before = true, called = false;
+
+        client._createManager();
+
+        client.manager.established = function() {
+          called = true;
+          assert(never_called_before);
+          never_called_before = false;
+        };
+
+        client.manager.emit('connect');
+
+        client._socket.emit('open');
+        client._socket.emit('open');
+
+        assert(called);
+      },
+
+      'should create a manager that stops listening to messages from a socket when the socket emits the close event': function() {
+        var called = false;
+
+        client._createManager();
+
+        client.manager.emit('connect');
+
+        client._socket.emit('open');
+
+        client._socket.on('message', function(data) {
+          var json = JSON.parse(data);
+          called = json.open;
+          assert(json.open);
+        });
+
+        client._socket.emit('message', '{"open":1}');
+
+        client._socket.emit('close');
+
+        client._socket.emit('message', '{"close":1}');
+
+        assert(called);
+      },
+
       'should create a manager that listens for the appropriate events': {
         'enterState': function() {
           var state = 'test',
@@ -693,6 +736,7 @@ exports.RadarClient = {
             client.manager.emit('connect');
 
             client._socket.emit('close');
+
             assert.ok(called);
           },
 
@@ -710,6 +754,7 @@ exports.RadarClient = {
             client.manager.emit('connect');
 
             client._socket.emit('message', message);
+
             assert.ok(called);
           }
         },
