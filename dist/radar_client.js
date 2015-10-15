a195964cdc7d51a25d3d16b005744d34b15ab737
Lucas Florio <lucasefe@gmail.com>
2015-10-14 18:57:34 -0700

    Update changelog and client_version

--- dist/radar_client.js
+++ dist/radar_client.js
@@ -752,7 +752,7 @@ module.exports = Client;
 "client_version.js": function(module, exports, require){
 // Auto-generated file, overwritten by scripts/add_package_version.js
 
-function getClientVersion() { return '0.14.5'; };
+function getClientVersion() { return '0.14.6'; };
 
 module.exports = getClientVersion;
 }
