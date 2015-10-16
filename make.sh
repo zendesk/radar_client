9be23980dda5d96dac4decbfb2d9dd991417addb
skalar <randomwag@gmail.com>
2015-10-16 15:27:19 +0200

    no make

--- make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./node_modules/gluejs/bin/gluejs --include ./lib \
-  --npm microee,sfsm \
-  --replace engine.io-client=eio,minilog=Minilog \
-  --global RadarClient \
-  --main lib/index.js \
-  --out dist/radar_client.js
