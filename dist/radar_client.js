39a08e0728f8c38d752301a229eef03758443de6
Lucas Florio <lucasefe@gmail.com>
2015-11-10 14:04:03 -0800

    Do not store presence offline on client state

--- dist/radar_client.js
+++ dist/radar_client.js
@@ -3,9 +3,8 @@ var r=function(){var e="function"==typeof require&&require,r=function(i,o,u){o||
 r.m[0] = {
 "minilog": { exports: Minilog },
 "engine.io-client": { exports: eio },
-"sfsm": {"c":4,"m":"state-machine.js"},
-"microee": {"c":5,"m":"index.js"},
-"radar_message": {"c":6,"m":"lib/index.js"},
+"sfsm": {"c":3,"m":"state-machine.js"},
+"microee": {"c":4,"m":"index.js"},
 "index.js": function(module, exports, require){
 var Client = require('./radar_client'),
     instance = new Client(),
@@ -639,7 +638,11 @@ Client.prototype._memorize = function(message) {
       return true;
     case 'set':
       if (message.to.substr(0, 'presence:/'.length) == 'presence:/') {
-        this._presences[message.to] = message.value;
+        if (message.value !== 'offline') {
+          this._presences[message.to] = message.value;
+        } else {
+          delete this._presences[message.to];
+        }
         return true;
       }
   }
@@ -766,10 +769,6 @@ r.m[2] = {
 "engine.io-client": { exports: eio },
 };
 r.m[3] = {
-"minilog": { exports: Minilog },
-"engine.io-client": { exports: eio },
-};
-r.m[4] = {
 "minilog": { exports: Minilog },
 "engine.io-client": { exports: eio },
 "package.json": function(module, exports, require){
@@ -998,7 +997,7 @@ var StateMachine = StateMachine = module.exports = {
 
 }
 };
-r.m[5] = {
+r.m[4] = {
 "minilog": { exports: Minilog },
 "engine.io-client": { exports: eio },
 "index.js": function(module, exports, require){
@@ -1092,324 +1091,6 @@ module.exports = {
   "_id": "microee@0.0.6",
   "_from": "microee@*"
 };
-}
-};
-r.m[6] = {
-"minilog": { exports: Minilog },
-"engine.io-client": { exports: eio },
-"package.json": function(module, exports, require){
-module.exports = {
-  "name": "radar_message",
-  "version": "1.0.1",
-  "description": "radar message api library",
-  "main": "lib/index.js",
-  "author": {
-    "name": "bolddane"
-  },
-  "license": "APACHE2",
-  "dependencies": {
-    "minilog": "*"
-  },
-  "devDependencies": {
-    "mocha": "*"
-  },
-  "scripts": {
-    "test": "ls ./test/*.test.js | xargs -n 1 -t -I {} sh -c 'TEST=\"{}\" npm run test-one'",
-    "test-one": "mocha --ui exports --reporter spec --slow 2000ms --bail \"$TEST\"",
-    "test-one-solo": "mocha --ui exports --reporter spec --slow 2000ms --bail"
-  },
-  "gitHead": "41a9e7a4fd440be7d3ff4a1df359f0bc2030db67",
-  "_id": "radar_message@1.0.1",
-  "_shasum": "acfba853478bc57bcfed9a922ababc8abc594c71",
-  "_from": "radar_message@1.0.1",
-  "_npmVersion": "2.1.4",
-  "_nodeVersion": "0.10.21",
-  "_npmUser": {
-    "name": "bolddane",
-    "email": "patrick_obrien53@yahoo.com"
-  },
-  "maintainers": [
-    {
-      "name": "bolddane",
-      "email": "patrick_obrien53@yahoo.com"
-    }
-  ],
-  "dist": {
-    "shasum": "acfba853478bc57bcfed9a922ababc8abc594c71",
-    "tarball": "http://registry.npmjs.org/radar_message/-/radar_message-1.0.1.tgz"
-  },
-  "directories": {},
-  "_resolved": "https://registry.npmjs.org/radar_message/-/radar_message-1.0.1.tgz"
-};
-},
-"lib/index.js": function(module, exports, require){
-var Request = require('./message_request.js'),
-    Response = require('./message_response.js'),
-    RadarMessage = function() {};
-
-RadarMessage.Request = Request;
-RadarMessage.Response = Response;
-
-module.exports = RadarMessage;
-
-},
-"lib/message_request.js": function(module, exports, require){
-var logger = require('minilog')('message:request');
-
-var opTable = {
-  control: ['nameSync'],
-  message: ['publish', 'subscribe', 'sync', 'unsubscribe'],
-  presence: ['get', 'set', 'subscribe', 'sync', 'unsubscribe'],
-  status: ['get', 'set', 'subscribe', 'sync', 'unsubscribe'],
-  stream: ['get', 'push', 'subscribe', 'sync', 'unsubscribe']
-};
-
-var Request = function (message) {
-  this.message = message;
-
-  if (!this._isValid()) {
-    logger.error('invalid request. op: ' + this.message.op + '; to: ' + this.message.to);
-    this.message = {};
-  }
-};
-
-Request.buildGet = function (scope, options) {
-  var message = { op: 'get', to: scope};
-  return new Request(message).setOptions(options);
-};
-
-Request.buildPublish = function (scope, value) {
-  var message = { op: 'publish', to: scope};
-  var request = new Request(message);
-  request.setAttr('value', value);
-
-  return request;
-};
-
-Request.buildPush = function (scope, resource, action, value) {
-  var message = { op: 'push', to: scope};
-  var request = new Request(message);
-  request.setAttr('resource', resource);
-  request.setAttr('action', action);
-  request.setAttr('value', value);
-
-  return request;
-};
-
-Request.buildNameSync = function (scope, options) {
-  var message = { op: 'nameSync', to: scope};
-  return new Request(message).setOptions(options);
-};
-
-Request.buildSet = function (scope, value, key, userType, clientData) {
-  var message = { op: 'set', to: scope};
-  var request = new Request(message);
-  request.setAttr('value', value);
-  request.setAttr('key', key);
-  request.setAttr('type', userType);
-  if (clientData) {
-    request.setAttr('clientData', clientData);
-  }
-
-  return request;
-};
-
-Request.buildSync = function (scope, options) {
-  var message = { op: 'sync', to: scope};
-  var request = new Request(message).setOptions(options);
-  if (request.isPresence()) {
-    request.forceV2Sync(options);
-  }
-  return request;
-};
-
-Request.buildSubscribe = function (scope, options) {
-  var message = { op: 'subscribe', to: scope};
-  return new Request(message).setOptions(options);
-};
-
-Request.buildUnsubscribe = function (scope, options) {
-  var message = { op: 'unsubscribe', to: scope};
-  return new Request(message);
-};
-
-// Instance methods
-
-Request.prototype.forceV2Sync = function (options) {
-  options = options || {};
-  options.version = 2;
-  this.setAttr('options', options);
-};
-
-Request.prototype.setAuthData = function (configuration) {
-  this.setAttr('userData', configuration.userData);
-  if (configuration.auth) {
-    this.setAttr('auth', configuration.auth);
-    this.setAttr('userId', configuration.userId);
-    this.setAttr('userType', configuration.userType);
-    this.setAttr('accountName', configuration.accountName);
-  }
-};
-
-Request.prototype.getMessage = function () {
-  return this.message;
-};
-
-Request.prototype.setOptions = function (options) {
-  // Keep check for options, since it is sometimes purposefully null
-  if (options) {
-    this.setAttr('options', options);
-  }
-
-  return this;
-};
-
-Request.prototype.isPresence = function () {
-  return this.type === 'presence';
-};
-
-Request.prototype.setAttr = function (keyName, keyValue) {
-  this.message[keyName] = keyValue;
-};
-
-Request.prototype.getAttr = function (keyName) {
-  return this.message[keyName];
-};
-
-Request.prototype.payload = function () {
-  return JSON.stringify(this.getMessage());
-};
-
-Request.prototype.getType = function () {
-  return this.type;
-};
-
-// Private methods
-
-Request.prototype._isValid = function () {
-  if (!this.message.op || !this.message.to) {
-    return false;
-  }
-
-  var type = this._getType();
-  if (type) {
-    if (this._isValidType(type) && this._isValidOperation(type)) {
-      this.type = type;
-      return true;
-    }
-  } else {
-    logger.error('missing type');
-  }
-  return false;
-};
-
-Request.prototype._isValidType = function (type) {
-  for (var key in opTable) {
-    if (opTable.hasOwnProperty(key) && key == type) {
-      return true;
-    }
-  }
-  this.errMsg = 'invalid type: ' + type;
-  logger.error(this.errMsg);
-  return false;
-};
-
-Request.prototype._isValidOperation = function (type) {
-  var ops = opTable[type];
-
-  var isValid = ops && ops.indexOf(this.message.op) >= 0;
-  if (!isValid) {
-    this.errMsg = 'invalid operation: ' + this.message.op + ' for type: ' + type;
-    logger.error(this.errMsg);
-  }
-  return isValid;
-};
-
-Request.prototype._getType = function () {
-  return this.message.to.substring(0, this.message.to.indexOf(':'));
-};
-
-module.exports = Request;
-
-},
-"lib/message_response.js": function(module, exports, require){
-var logger = require('minilog')('message:response');
-
-function Response (message) {
-  this.message = message;
-
-  if (!this._validate()) {
-    logger.error('invalid response. message: ' + JSON.stringify(message));
-    this.message = {};
-  }
-}
-
-Response.prototype.getMessage = function () {
-  return this.message;
-};
-
-Response.prototype._validate = function () {
-  if (!this.message.op) {
-    this.errMsg = 'missing op';
-    return false;
-  }
-
-  switch(this.message.op) {
-    case 'ack':
-      if (!this.message.value) {
-        this.errMsg = 'missing value';
-        logger.error(this.errMsg);
-        return false;
-      }
-      break;
-
-    default:
-      if (this.message.op != 'err' && !this.message.to) {
-        this.errMsg = 'missing to';
-        logger.error(this.errMsg);
-        return false;
-      }
-  }
-
-  return true;
-};
-
-Response.prototype.isValid = function () {
-  return !!this.message.to && !!this.message.value && !!this.message.time;
-};
-
-Response.prototype.isFor = function (request) {
-  return this.getAttr('to') === request.getAttr('to');
-};
-
-Response.prototype.isAckFor = function (request) {
-  return this.getAttr('value') === request.getAttr('ack');
-};
-
-Response.prototype.getAttr = function (attr) {
-  return this.message[attr];
-};
-
-Response.prototype.forceV1Response = function () {
-  // Sync v1 for presence scopes is inconsistent: the result should be a 'get'
-  // message, but instead is an 'online' message.  Take a v2 response and
-  // massage it to v1 format prior to returning to the caller.
-  var message = this.message, value = {}, userId;
-  for (userId in message.value) {
-    if (message.value.hasOwnProperty(userId)) {
-      // Skip when not defined; causes exception in FF for 'Work Offline'
-      if (!message.value[userId]) { continue; }
-      value[userId] = message.value[userId].userType;
-    }
-  }
-  message.value = value;
-  message.op = 'online';
-
-  this.message = message;
-};
-
-module.exports = Response;
-
 }
 };
 RadarClient = r("index.js");}());
--- tests/radar_client.unit.test.js
+++ tests/radar_client.unit.test.js
@@ -465,8 +465,7 @@ exports.RadarClient = {
         assert.equal(1, Object.keys(client._presences).length);
 
         client._memorize({ op: 'set', to: 'presence:/foo/bar', value: 'offline' });
-        assert.equal(1, Object.keys(client._presences).length);
-        assert.equal('offline', client._presences['presence:/foo/bar']);
+        assert.equal(0, Object.keys(client._presences).length);
         done();
       },
 
@@ -527,16 +526,11 @@ exports.RadarClient = {
         client._restoreRequired = true;
         client.configure({ accountName: 'foo', userId: 123, userType: 2 });
         client.alloc('test', function() {
-          assert.equal(MockEngine.current._written.length, 3);
+          assert.equal(MockEngine.current._written.length, 2);
           assert.ok(MockEngine.current._written.some(function(message) {
             return (message.op == 'set' &&
               message.to == 'presence:/foo/bar' &&
               message.value == 'online');
-          }));
-          assert.ok(MockEngine.current._written.some(function(message) {
-            return (message.op == 'set' &&
-              message.to == 'presence:/foo/bar2' &&
-              message.value == 'offline');
           }));
           done();
         });
