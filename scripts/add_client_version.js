9043fc16fb252a99f1928429a005c9a49c9eee72
jden <jden@zendesk.com>
2016-02-18 12:00:18 -0800

    Reformat to standardjs
    Add standard to npm pretest script
    
    Update client_version template
    
    Fix typo in tests

--- scripts/add_client_version.js
+++ scripts/add_client_version.js
@@ -1,25 +1,25 @@
 // Add the package version to radar_client.js prior to building dist
-var version = require('../package.json').version,
-    fs = require('fs');
+var version = require('../package.json').version
+var fs = require('fs')
 
-var FILEPATH = 'lib/client_version.js';
+var FILEPATH = 'lib/client_version.js'
 
 // Create the string to write
-var fileContents = '// Auto-generated file, overwritten by'
-                    + ' scripts/add_package_version.js\n\n'
-                    + 'function getClientVersion() { return \''
-                    + version
-                    + '\'; };\n\n'
-                    + 'module.exports = getClientVersion;';
+var fileContents = '// Auto-generated file, overwritten by' +
+  ' scripts/add_package_version.js\n\n' +
+  "function getClientVersion () { return '" +
+  version +
+  "' }\n\n" +
+  'module.exports = getClientVersion\n'
 
 // Rewrite the file lib/version.js to contain a current getVersion()
 if (fs.exists(FILEPATH)) {
-  fs.unlinkSync(FILEPATH);
+  fs.unlinkSync(FILEPATH)
 }
 
 // Write the client version to a new instance of the file
-var stream = fs.createWriteStream(FILEPATH);
+var stream = fs.createWriteStream(FILEPATH)
 stream.once('open', function () {
-  stream.write(fileContents);
+  stream.write(fileContents)
   stream.end()
-});
+})
--- tests/state.test.js
+++ tests/state.test.js
@@ -1,150 +1,149 @@
-var assert = require('assert'),
-    log = require('minilog')('state.test'),
-    StateMachine = require('../lib/state.js'),
-    machine,
-    sinon = require('sinon'),
-    clock;
+var assert = require('assert')
+var StateMachine = require('../lib/state.js')
+var machine
+var sinon = require('sinon')
+var clock
 
 exports['given a state machine'] = {
-  beforeEach: function() {
-    machine = StateMachine.create();
-    clock = sinon.useFakeTimers();
+  beforeEach: function () {
+    machine = StateMachine.create()
+    clock = sinon.useFakeTimers()
   },
 
-  afterEach: function() {
-    clock.restore();
+  afterEach: function () {
+    clock.restore()
   },
 
-  'calling start twice should not cause two connections': function(done) {
-    var connecting = false;
+  'calling start twice should not cause two connections': function (done) {
+    var connecting = false
 
-    machine.on('connect', function() {
-      assert.ok(!connecting);
-      connecting = true;
-    });
-    machine.start();
-    assert.ok(machine.is('connecting'));
-    machine.on('established', function() {
-      assert.ok(machine.is('authenticating'));
-      machine.activate();
-      machine.start();
-      assert.ok(machine.is('activated'));
-      done();
-    });
-    machine.established();
+    machine.on('connect', function () {
+      assert.ok(!connecting)
+      connecting = true
+    })
+    machine.start()
+    assert.ok(machine.is('connecting'))
+    machine.on('established', function () {
+      assert.ok(machine.is('authenticating'))
+      machine.activate()
+      machine.start()
+      assert.ok(machine.is('activated'))
+      done()
+    })
+    machine.established()
   },
 
-  'if the user calls disconnect the machine will reconnect after a delay': function(done) {
-    this.timeout(4000);
+  'if the user calls disconnect the machine will reconnect after a delay': function (done) {
+    this.timeout(4000)
 
-    machine.open();
-    machine.connect();
-    assert.ok(machine.is('connecting'));
-    machine.once('connect', function() {
-      machine.close();
-      done();
-    });
-    machine.disconnect();
-    clock.tick(2500);
+    machine.open()
+    machine.connect()
+    assert.ok(machine.is('connecting'))
+    machine.once('connect', function () {
+      machine.close()
+      done()
+    })
+    machine.disconnect()
+    clock.tick(2500)
   },
 
-  'the first connection should begin connecting, after disconnected it should automatically reconnect': function(done) {
-    this.timeout(4000);
-    machine.open();
-    machine.connect();
-    assert.ok(machine.is('connecting'));
+  'the first connection should begin connecting, after disconnected it should automatically reconnect': function (done) {
+    this.timeout(4000)
+    machine.open()
+    machine.connect()
+    assert.ok(machine.is('connecting'))
 
-    var disconnected = false;
+    var disconnected = false
 
-    machine.once('disconnected', function() {
-      disconnected = true;
-    });
+    machine.once('disconnected', function () {
+      disconnected = true
+    })
 
-    machine.once('connect', function() {
-      assert.ok(disconnected);
-      done();
-    });
+    machine.once('connect', function () {
+      assert.ok(disconnected)
+      done()
+    })
 
-    machine.disconnect();
-    clock.tick(2500);
+    machine.disconnect()
+    clock.tick(2500)
   },
 
-  'connections that hang should be detected after 10 seconds': function(done) {
-    this.timeout(14000);
+  'connections that hang should be detected after 10 seconds': function (done) {
+    this.timeout(14000)
 
-    machine.disconnect = function() {
-      done();
-    };
+    machine.disconnect = function () {
+      done()
+    }
 
-    machine.connect();
-    clock.tick(10001);
+    machine.connect()
+    clock.tick(10001)
   },
 
-  'should not get caught by timeout if connect fails for different reasons' : function(done) {
-    var once = true;
-    this.timeout(20000);
-    var disconnects = 0;
+  'should not get caught by timeout if connect fails for different reasons': function (done) {
+    var once = true
+    this.timeout(20000)
+    var disconnects = 0
 
-    machine.on('disconnect', function() {
-      disconnects++;
-    });
+    machine.on('disconnect', function () {
+      disconnects++
+    })
 
-    setTimeout(function() {
+    setTimeout(function () {
       // Only 1 disconnect due to manager.disconnect()
-      assert.equal(disconnects, 1);
-      done();
-    }, 15000);
+      assert.equal(disconnects, 1)
+      done()
+    }, 15000)
 
-    machine.on('connect', function() {
+    machine.on('connect', function () {
       if (once) {
-        machine.disconnect();
-        once = false;
-      }else {
-        machine.established();
+        machine.disconnect()
+        once = false
+      } else {
+        machine.established()
       }
-    });
-    machine.connect();
+    })
+    machine.connect()
 
-    clock.tick(15001);
+    clock.tick(15001)
   },
 
-  'connections that fail should cause exponential backoff, finally emit unavailable': function(done) {
-    this.timeout(65000);
+  'connections that fail should cause exponential backoff, finally emit unavailable': function (done) {
+    this.timeout(65000)
 
-    var available = true, tries = 10;
+    var available = true
+    var tries = 10
 
-    machine.open();
+    machine.open()
 
-    machine.on('unavailable', function() {
-      available = false;
-      done();
-    });
+    machine.on('unavailable', function () {
+      available = false
+      done()
+    })
 
-    machine.on('connecting', function() {
+    machine.on('connecting', function () {
       if (available && --tries) {
-        machine.disconnect();
+        machine.disconnect()
       }
-    });
+    })
 
-    machine.connect();
+    machine.connect()
 
-    clock.tick(64000);
-
+    clock.tick(64000)
   },
 
-  'closing will cancel the guard timer': function() {
-    machine.open();
-    assert(!machine._guard);
-    machine.connect();
-    assert(machine._guard);
-    machine.close();
-    assert(!machine._guard);
+  'closing will cancel the guard timer': function () {
+    machine.open()
+    assert(!machine._guard)
+    machine.connect()
+    assert(machine._guard)
+    machine.close()
+    assert(!machine._guard)
   }
-};
+}
 
 // When this module is the script being run, run the tests:
-if (module == require.main) {
-  var mocha = require('child_process').spawn('mocha', [ '--colors', '--ui', 'exports', '--reporter', 'spec', __filename ]);
-  mocha.stdout.pipe(process.stdout);
-  mocha.stderr.pipe(process.stderr);
+if (module === require.main) {
+  var mocha = require('child_process').spawn('mocha', [ '--colors', '--ui', 'exports', '--reporter', 'spec', __filename ])
+  mocha.stdout.pipe(process.stdout)
+  mocha.stderr.pipe(process.stderr)
 }
