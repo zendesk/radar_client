92b0439c16f33c6f3abc9fcf9bf368cb1d33f15f
jden <jden@zendesk.com>
2016-02-23 15:43:46 -0800

    Loosen version range on radar_message dependency

--- dist/radar_client.js
+++ dist/radar_client.js
@@ -1076,7 +1076,7 @@ var RadarClient =
 
 	// Auto-generated file, overwritten by scripts/add_package_version.js
 
-	function getClientVersion () { return '0.15.1' }
+	function getClientVersion () { return '0.15.2' }
 
 	module.exports = getClientVersion
 
